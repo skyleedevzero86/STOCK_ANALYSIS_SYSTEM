9f775bbe0fcf91f9d903d9b202777583a9151332
이경용 <sleekydz86@icloud.com>
2025-10-22 19:09:48 +0900

    backend 셋팅

---
+++ webbackend/src/main/kotlin/com/sleekydz86/backend/WebbackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebbackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebbackendApplication>(*args)
+}
